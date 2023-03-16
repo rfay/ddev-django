f28590f87404a1affb39d4c3340188bb4e49770f
Randy Fay <randy@randyfay.com>
2023-03-16 13:13:31 -0600

    Refactor ddev config, handle static files properly

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf import settings
+from django.contrib.staticfiles import views
+from django.urls import re_path
+
+
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', views.serve),
+    ]
